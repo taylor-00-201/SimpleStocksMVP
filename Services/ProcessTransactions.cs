68d9dad39d1df064cbc6991f9d5c958e84f4d4c3
Taylor <taylor00201@gmail.com>
2023-06-02 17:16:15 -0500

    added basic service class

--- Services/ProcessTransactions.cs
+++ Services/ProcessTransactions.cs
@@ -1,55 +1,69 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Authentication;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using SimpleStocks.Interfaces;
 using SimpleStocks.Repositories;
+using SimpleStocks.Models;
 
 
 namespace SimpleStocks.Services
 {
     public class ProcessTransactions
     {
+
+        private readonly IAssetRepository _assetRepository;
+        private readonly IStockUserRepository _stockUserRepository;
+        private readonly ITransactionsRepository _transactionsRepository;
+
+        public ProcessTransactions(IAssetRepository assetRepository, IStockUserRepository stockUserRepository, ITransactionsRepository transactionsRepository) 
+        {
+            _assetRepository = assetRepository;
+            _stockUserRepository = stockUserRepository;
+            _transactionsRepository = transactionsRepository;
+        }
+        
         public static string AddAnAssetEntryPoint(string values) 
         {
             try
             {   
                 JObject value = JObject.Parse(values);
                 string? AssetId = value["Id"].ToString();
-                string? Symbol = value["symbol"].ToString();
-                string? name = value["name"].ToString();
-                string? price = value["currentPrice"].ToString();
+                string? symbol = value["symbol"].ToString();
+                string? Name = value["Name"].ToString();
+                string? Price = value["CurrentPrice"].ToString();
+                
+
+                Transactions transactions = new Transactions();
+                _transactionsRepository.ProcessTransaction(transactions);
+
+                BankAccounts bankAccounts = new BankAccounts();
+                if (transactions.TransactionType == "Buy")
+                {
+                    _stockUserRepository.SubtractFromBankAccount(bankAccounts);
+                }
+                else 
+                {
+                    _stockUserRepository.AddToBankAccount(bankAccounts);
+                }
+
+
+                Asset asset = new Asset 
+                {
+                    Id = AssetId, 
+                    Symbol = symbol, 
+                    Name = Name,
+                    CurrentPrice = Price
+                }
+                _assetRepository.UpdateAsset(asset, assetId)
 
             }
-            catch (Exception)
+            catch (Exception Ex)
             {
 
-                throw;
+                Console.WriteLine("Error");
             }
             
             
-            return string.Empty;
-        }
-
-        private static string AddTransation()
-        {
-
-
-
-            return string.Empty;
-        }
-
-        private static string ModifyUserBlance()
-        {
-
-
-
-            return string.Empty;
-        }
-
-
-        private static string AddAsset() 
-        {
-
-
-
             return string.Empty;
         }
     }
