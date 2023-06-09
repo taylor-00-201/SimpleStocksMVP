0d02b8abdcc1347ee19e0b1de427dd9d9b65ca4b
taylo <taylor00201@gmail.com>
2023-06-09 11:54:50 -0500

    stocks and transactions on screen

--- SimpleStocksClient/simplestocks/src/Components/BuyStock.js
+++ SimpleStocksClient/simplestocks/src/Components/BuyStock.js
@@ -14,23 +14,6 @@ export const BuyStock = () => {
   const StockUser = JSON.parse(localStorage.getItem("StockUser"));
   const stockUserId = StockUser ? StockUser.id : null;
 
-  // const returnUserTransactions = () => {
-  //   return (
-  //     <div>
-  //       {parsedTransactions.map((transaction) => {
-  //         return (
-  //           <div key={transaction.datetime}>
-  //             <h1>Transaction Type: {transaction.transactiontype}</h1>
-  //             <h1>Quantity: {transaction.quantity}</h1>
-  //             <h1>Asset Id: {transaction.assetId}</h1>
-  //             <h1>Date: {transaction.datetime}</h1>
-  //           </div>
-  //         );
-  //       })}
-  //     </div>
-  //   );
-  // };
-
   const fetchUserTransactions = async (stockUserId) => {
     const apiResponse = await fetch(
       `https://localhost:7043/api/Transactions/TransactionByIdAll?Id=${stockUserId}`
@@ -84,8 +67,8 @@ export const BuyStock = () => {
 
   return (
     <div>
-      {/* {stocks.length !== 0 &&
-        stocks.map((stock, index) => <StockCard index={index} stock={stock} />)} */}
+      {stocks.length !== 0 &&
+        stocks.map((stock, index) => <StockCard index={index} stock={stock} />)}
       <div>{<BuyStockEngine />}</div>
       <div>
         {matchingStockTransaction.length !== 0 &&
